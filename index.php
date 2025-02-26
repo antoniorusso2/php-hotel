6b48516d786a618446de6f078150d8004f1e3598
antoniorusso2 <russoantonio303@outlook.it>
2025-02-26 15:31:39 +0100

    add bootstrap5 and table for hotels data

--- index.php
+++ index.php
@@ -52,33 +52,62 @@ $hotels = [
 </head>
 
 <body>
-    <header>
-        <h1>
+    <header class="header">
+        <h1 class="title text-center">
             PHP Hotel
         </h1>
     </header>
-    <main>
+    <main class="main my-5">
         <div class="container">
+            <table class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <!-- manual headers for using italian language -->
+                        <th scope="col">Hotel</th>
+                        <th scope="col">Descrizione</th>
+                        <th scope="col" class="text-center">Parcheggio</th>
+                        <th scope="col">Voto</th>
+                        <th scope="col">Distanza dal centro</th>
+                    </tr>
 
-            <?php
-            $index = 0;
-            foreach ($hotels as $hotel) {
-                $index++;
-                //class for every hotel
-                $class = "hotel$index";
+                    <!-- dynamic headers using for each-->
+                    <?php
+                    // foreach ($hotels[0] as $key => $value) {
+                    //     echo "<th scope='col text-center'>$key</th>";
+                    // }
+                    ?>
+                </thead>
+                <tbody>
+                    <!-- hotels list data -->
+                    <?php
+                    //parking checkmark
+                    $checkmark = "&#10003;";
+                    $not_available = "&#10005;";
 
-                echo "<ul class=$class>";
+                    foreach ($hotels as $hotel) {
 
-                //list of properties for every hotel
-                foreach ($hotel as $key => $value) {
-                    echo "<li>$key: $value </li>";
-                }
+                        echo "<tr>";
 
-                echo "</ul>";
-            }
-            ?>
+                        foreach ($hotel as $key => $value) {
+                            if ($key == 'parking') {
+                                if ($value) {
+                                    echo "<td class='text-success text-center fw-bold'>$checkmark</td>";
+                                } else {
+                                    echo "<td class='text-danger text-center fw-bold'>$not_available</td>";
+                                }
+                            } else {
+                                echo "<td>$value</td>";
+                            }
+                        }
+                        echo "</tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
         </div>
     </main>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
